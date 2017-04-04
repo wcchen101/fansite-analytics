3ced111cc9543fedeca8abb5f5626728b9322769
Wei-Chung Chen <wcchen101@gmail.com>
2017-04-04 11:49:31 -0700

    add helper function and refine all feature function
    1. add time conversion helper function to convert timestamp to date time
    2. add time difference helper function to get the difference of the
    time stamp for certain timeline
    3.  refine sliding windows algorithm
    4. refine detect anomaly login algorithm

--- src/process_log.py
+++ src/process_log.py
@@ -3,6 +3,8 @@ import re
 import time, datetime
 import os, sys
 from tempfile import mkstemp
+from datetime import datetime
+import calendar
 heap = []
 cwd = os.getcwd()
 parentPath = os.path.abspath(os.path.join(cwd, os.pardir))
@@ -10,15 +12,15 @@ tempPath = parentPath +'/temp'
 
 def main():
     bufferSize = 50000
-    inputFile = open(tempPath+ '/'+ 'log_input/'+'log.txt', 'r')
-    blockedFile = open('blocked.txt', 'w')
-    hoursFile = open('hours.txt', 'w')
-    hostFile = open(tempPath + '/'+ 'log_output/'+'hosts.txt','w')
-    resourceFile = open(tempPath+ '/'+ 'log_output/'+ 'resources.txt', 'w')
+    inputFile = open(parentPath+ '/'+ 'log_input/'+'log.txt', 'r')
+    blockedFile = open(parentPath + '/'+ 'log_output/' + 'blocked.txt', 'w')
+    hoursFile = open(parentPath + '/'+ 'log_output/' + 'hours.txt', 'w')
+    hostFile = open(parentPath + '/'+ 'log_output/'+'hosts.txt','w')
+    resourceFile = open(parentPath+ '/'+ 'log_output/'+ 'resources.txt', 'w')
     dict_ip = {}
     dict_resource = {}
     dict_time = {}
-    block_list = {}
+    blocklist = {}
     time_arr = []
     whole_arr = []
     timewindow = 60
@@ -35,7 +37,6 @@ def main():
                 #feature 1
                 if match_ip:
                     ip = match_ip.group()
-                    print(ip)
                     if ip not in dict_ip:
                         dict_ip[ip] = 1
                     else:
@@ -50,39 +51,39 @@ def main():
                         dict_resource[resource] = int(bandwidth)
                     else:
                         dict_resource[resource] += int(bandwidth)
+
                 #feature 3
                 if match_time and match_ip and match_resource:
                     time, ip, resource = match_time.group(), match_ip.group(), match_resource.group()
+                    # print(time[1:-1])
                     whole_arr.append(words)
                     time_arr.append([time[1:-1], ip])
                 print('.', end = '')
 
-        except UnicodeDecodeError:
+        except (UnicodeDecodeError, IndexError):
             print('wrong format or something else')
-            continue
+            pass
         else:
             print('Done')
             break
 
     #feature1
     getTopk(dict_ip, 10, hostFile, True)
-    print(dict_ip)
     #feature2
     getTopk(dict_resource, 10, resourceFile, False)
-    print(dict_resource)
-    #feature3
-    # slidingWindow(dict_time, time_arr, 60)
-    # getTopk(dict_time, 10, hoursFile)
+    # feature3
+    slidingWindow(dict_time, time_arr, 3600)
+    getTopk(dict_time, 10, hoursFile, True)
     #feature4
-    # detectLogInAnomly(blocklist, time_arr, 20, whole_arr, blockedFile)
+    detectLogInAnomly(blocklist, time_arr, 20, whole_arr, blockedFile)
     print('done all')
 
 def getTopk(dict, k, filename, writeCount=True):
     heap = [(-value, key) for key, value in dict.items()]
     heapq.heapify(heap)
     topk_arr = []
-
-    for i in range(len(dict)):
+    minLen = min(len(dict), k)
+    for i in range(minLen):
         topkelement = heapq.heappop(heap)
         topk_arr.append((topkelement[1], -topkelement[0]))
         if writeCount is False:
@@ -99,59 +100,69 @@ def getTime(timeinput):
     timestamp = time.mktime(time.strptime(timeinput[0], '%d/%b/%Y:%H:%M:%S'))
     return timestamp
 
+def convertTimeStamp(timestamp):
+    timeoutput = datetime.fromtimestamp(timestamp).strftime('%d/%b/%Y:%H:%M:%S -0400')
+    return timeoutput
+
+def timeDiff(timebefore, timeafter):
+    timeDifference = getTime(timeafter) - getTime(timebefore)
+    return timeDifference
 
 def slidingWindow(dict_time, time_arr, window):
+    start = getTime(time_arr[0][0])
+    stop = getTime(time_arr[-1][0])
     i = 0
-    while i < len(time_arr):
-        val1 = getTime(time_arr[i][0])
-        dict_time[time_arr[i][0]] = 1
-        j = i + 1
-        while j < len(time_arr):
-            val2 = getTime(time_arr[j][0])
-            if val2 - val1 > window:
-                break
-            else:
-                dict_time[time_arr[i][0]] += 1
-            print('.', end = '')
+    prevtime = time_arr[0][0]
+    while start <= stop:
+        startTime = convertTimeStamp(start)
+        dict_time[startTime] = 0
+        if i + 1 < len(time_arr) and start > getTime(prevtime):
+            i += 1
+            prevtime = time_arr[i][0]
+
+        j = i
+        while j < len(time_arr) and getTime(time_arr[j][0]) - start < window:
+            dict_time[startTime] += 1
             j += 1
-        i += 1
+            print('.', end = '')
+        start += 1
     print('Done sliding window')
 
 def detectLogInAnomly(blocklist, time_arr, window, whole_arr, blockedFile):
-    i, j = 0, 1
+    i = 0
     while i < len(time_arr):
         ip1 = whole_arr[i][0]
         protocol1 = whole_arr[i][-2]
-        val1 = getTime(time_arr[i][0])
-        if protocol1 == '200':
+        if protocol1 == '200' and ip1 not in blocklist:
             i += 1
             continue
-        #logIn fails
-        tryCount = 1
-        blocklist[time_arr[i][0]] = (time_arr[i][0], tryCount)
-        j = i + 1
-        while j < len(time_arr):
-            val2 = getTime(time_arr[j][0])
-            ip2 = whole_arr[j][0]
-            protocol2 = whole_arr[j][-2]
-            if val2 - val1 > window:
-                del blocklist[time_arr[i][0]]
-                break
-            elif val2 - val1 < window and ip1 == ip2:
-                if protocol2 == '200' and time_arr[i][0] in blocklist:
-                    del blocklist[time_arr[i][0]]
-                    tryCount = 0
-                    break
-
-                elif protocol2 != '200':
-                    tryCount += 1
-                    blocklist[time_arr[i][0]] = (time_arr[i][0], tryCount)
-                    if tryCount > 3:
-                        #write ip block to the file
-                        blockedFile.write(' '.join(whole_arr[j]) + '\n')
-            j += 1
-            # print('.', end = '')
+        elif protocol1 == '200' and ip1 in blocklist and timeDiff(blocklist[ip1][0], time_arr[i][0]) > window:
+            del blocklist[ip1]
+            i += 1
+            continue
+        elif protocol1 == '200' and ip1 in blocklist and timeDiff(blocklist[ip1][0], time_arr[i][0]) < window:
+            if blocklist[ip1][1] >= 3:
+                blockedFile.write(' '.join(whole_arr[i]) + '\n')
+            i += 1
+            continue
+        elif protocol1 != '200' and ip1 in blocklist and timeDiff(blocklist[ip1][0], time_arr[i][0]) > window:
+            del blocklist[ip1]
+            blocklist[ip1] = [time_arr[i][0], 1]
+            i += 1
+            continue
+        elif protocol1 != '200' and ip1 not in blocklist:
+            blocklist[ip1] = [time_arr[i][0], 1]
+            i += 1
+            continue
+        elif protocol1 != '200' and ip1 in blocklist and timeDiff(blocklist[ip1][0], time_arr[i][0]) < window:
+            tryCount = blocklist[ip1][1] + 1
+            blocklist[ip1][1] = tryCount
+            if tryCount > 3:
+                blockedFile.write(' '.join(whole_arr[i]) + '\n')
+            i += 1
+            continue
         i += 1
+
     print('done detect anomly log')
 
 if __name__ == '__main__':
